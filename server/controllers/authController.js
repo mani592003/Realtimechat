bb82ea42295fd5eec1a48e63f74f81b14f9c519f
mani592003 <chevurisaimanikanta@gmail.com>
2025-07-30 22:37:49 +0530

    project added

---
+++ server/controllers/authController.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+  res.status(201).json({ message: "User registered" });
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  res.json({ token, user: { username: user.username } });
+};
